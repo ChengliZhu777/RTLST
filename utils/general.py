02879021d4aea16885749fd37b201e11226a1cb5
ChengliZhu777 <154007802+ChengliZhu777@users.noreply.github.com>
2024-03-05 20:45:19 +0800

    Update general.py

--- utils/general.py
+++ utils/general.py
@@ -1,1 +1,11 @@
+import logging
+import argparse
 
+
+def set_logging(rank=-1):
+    """
+    %(message)s : output log content
+    %(asctime)s : output logging time
+    """
+    logging.basicConfig(format='%(message)s',
+                        level=logging.INFO if rank in [-1, 0] else logging.WARN)
