3bb10d6ee4ea79a5d0e16a078a3ecb443afbbb10
ChengliZhu777 <154007802+ChengliZhu777@users.noreply.github.com>
2024-05-09 22:32:51 +0800

    Update resnet.py

--- model/backbone/resnet.py
+++ model/backbone/resnet.py
@@ -1,1 +1,9 @@
+import torch.nn as nn
 
+
+class ResNet(nn.Module):
+    def __init__(self, config):
+        super(ResNet, self).__init__()
+
+        self.model, self.save_layers = parse_model(config, module_name='ResNet-6')
+      
