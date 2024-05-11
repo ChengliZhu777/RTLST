ebbe1453a5a0318e38a050257a97b5e5b70b6c73
ChengliZhu777 <154007802+ChengliZhu777@users.noreply.github.com>
2024-05-11 20:42:12 +0800

    Update common.py

--- model/common.py
+++ model/common.py
@@ -1,6 +1,12 @@
 import torch.nn as nn
 
 
+def auto_padding(kernel_size, padding=None):
+    if padding is None:
+        padding = kernel_size // 2 if isinstance(kernel_size, int) else tuple([ks // 2 for ks in kernel_size])
+    return padding
+
+
 class Conv(nn.Module):
     def __init__(self, input_channels, output_channels, kernel_size, stride,
                  padding=None, groups=1, bias=True, bn=False, act=None):
@@ -19,4 +25,14 @@ class Conv(nn.Module):
         if self.act is not None:
             out = self.act(out)
         return out
-      
+
+
+class MaxPool(nn.Module):
+    def __init__(self, kernel_size, stride, padding=0, dilation=1):
+        super(MaxPool, self).__init__()
+        self.max_pool = nn.MaxPool2d(kernel_size=kernel_size, stride=stride,
+                                     padding=padding, dilation=dilation)
+
+    def forward(self, x):
+        return self.max_pool(x)
+        
