40d461458b037b6fa1d559718e7d216e9e79e3e6
ChengliZhu777 <154007802+ChengliZhu777@users.noreply.github.com>
2024-05-16 23:37:10 +0800

    Update rtlstd.py

--- model/rtlstd.py
+++ model/rtlstd.py
@@ -1,8 +1,10 @@
 class RTLSTD(nn.Module):
     def __init__(self, patch_size, backbone, rpn):
         super(RTLSTD, self).__init__()
-      self.encoder = build_model(backbone)
+        self.patch_size = patch_size
+        self.encoder = build_model(backbone)
+        self.patch_rpn = build_model(rpn)
 
     def forward(self, x):
-        rpn = self.encoder(patch_images)
+        rpn = self.patch_rpn(self.encoder(patch_images))
         
